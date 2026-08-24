5e1cd899f9811ec4f739d0e9f69e554f50f33147
Marketif <baouddiabdellatif@gmail.com>
2026-08-24 20:27:52 +0200

    chore: remove local scratch scripts that broke Nixpacks build

--- fix.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); let c = fs.readFileSync('src/app/dashboard/[slug]/page.tsx', 'utf8'); c = c.replace(/\\} Weiter so!/g, '}'); fs.writeFileSync('src/app/dashboard/[slug]/page.tsx', c);
--- fix_merchant.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); let c = fs.readFileSync('src/locales/merchant.ts', 'utf8'); c = c.split('\\\\n').join('\n'); fs.writeFileSync('src/locales/merchant.ts', c);
