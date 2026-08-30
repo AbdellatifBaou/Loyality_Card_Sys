f28708981e23fff3dd7cad58f8c5f44607c6ba6e
Marketif <baouddiabdellatif@gmail.com>
2026-08-30 14:33:12 +0200

    chore: rm temp script

--- update-pin-frontend.js
+++
@@ -1,7 +0,0 @@
-﻿
-const fs = require("fs");
-let code = fs.readFileSync("src/app/dashboard/[slug]/page.tsx", "utf8");
-code = code.replace(/minLength=\{4\}/g, `minLength={4} maxLength={4} pattern="\\d{4}"`);
-code = code.replace(/maxLength=\{6\}/g, `maxLength={4} pattern="\\d{4}"`);
-fs.writeFileSync("src/app/dashboard/[slug]/page.tsx", code);
-
